533bc936584fdef4501514a6918731787eba2f1a
Roksana Kanicka <97945@student.ue.poznan.pl>
2025-03-10 10:27:09 +0100

    Zadanie zrobione

---
+++ zadanie1.py
@@ -0,0 +1,12 @@
+# Dane wejściowe (czasy w minutach)
+czasy = [43, 51, 46]
+
+# Inicjalizowanie sumy
+suma = sum(czasy)
+print(suma)
+
+# Obliczanie średniej
+srednia = suma / len(czasy)
+
+# Wyświetlanie wyniku
+print(f"Średnia długość odcinka: {srednia:.2f} minut")
